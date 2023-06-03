57770bba7d79d313f9f8e0c32dcf3cbd2397b7c8
WrightChristian-FS <83625690+WrightChristian-FS@users.noreply.github.com>
2023-06-02 22:34:32 -0700

    Created the Course Application

---
+++ Building RESTful API /index.js
@@ -0,0 +1,62 @@
+const express = require('express'); 
+const app = express(); 
+const quizessCtrl = require("./src/controllers/quizzes");
+const questionsCtrl = require("./src/controllers/questions");
+const choicesCtrl = require("./src/controllers/choices");
+
+
+app.get("/products", (request, response) => { 
+    response.send("All Products... ")
+})
+
+app.use("/quizzes", quizessCtrl)
+app.use("/questions", questionsCtrl)
+app.use("/choices", choicesCtrl)
+
+
+
+
+
+app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get("/", (request, response) => { 
+//     response.send("Home Page! Get... ")
+// })
+
+// app.post("/", (request, response) => { 
+//     response.send("Home PAge! POST...")
+// })
+
+// app.get("/products/:productName", (request, response) => { 
+//     response.send("Product Page! Product Name: " + request.params.productName); 
+// })
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# AdvancedSSL
-Advanced SSL Course 
+# Advanced Server Side Language Course 
+---
+
+## Building RESTful API 
+This assignment is to build a simple REST API that demostrates the different stages of CRUD
